17abaa1f0c63b71ef12740d19b5a1e9d47094ee6
TarsorCase <gerardgonzalez41@gmail.com>
2024-05-28 14:24:59 -0400

    Subiendo documentos

---
+++ Calculadora/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestDemo;
+
+Console.WriteLine("Calculadora");
+Console.Write("Ingrese un valor para chekar si es par: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Mathematics.IsEvenNumber(a));
---
+++ TestDemo/Mathematics.cs
@@ -0,0 +1,9 @@
+﻿namespace TestDemo;
+
+public static class Mathematics
+{
+ public static bool IsEvenNumber(int num) {  
+ 
+ return num % 2 == 0 ? true : false;
+ }
+}
